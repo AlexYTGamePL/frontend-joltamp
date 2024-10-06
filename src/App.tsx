9124af76e2e927af6bdcde478af4fe45bd32c760
rraix <166999952+rraix0@users.noreply.github.com>
2024-10-06 18:37:00 +0200

    v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 // App.tsx
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginSite from './site/login';
-import RegisterSite from './site/register';
+import LoginSite from './routs/login/login.tsx';
+import RegisterSite from './routs/register/register.tsx';
+import WelcomeSite from './routs/welcome/welcome.tsx';
+import Site404 from './routs/404/404.tsx';
+import Panel from './routs/panel/panel.tsx';
 
 function App() {
   return (
-        <Router>
-
-      {/* Definiowanie ścieżek do wyświetlania komponentów */}
+    <Router>
       <Routes>
+        <Route path="/" element={<WelcomeSite />} />
+        <Route path="*" element={<Site404 />} />
+        <Route path="/panel" element={<Panel />} />
         <Route path="/login" element={<LoginSite />} />
         <Route path="/register" element={<RegisterSite />} />
       </Routes>
--- src/routs/login/login.tsx
+++ src/routs/login/login.tsx
@@ -1,19 +1,44 @@
-import '../styles/login.css';
-import React, { useState } from 'react';
+//import axios from 'axios';
+
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function LoginSite() {
+    useEffect(() => {
+        import('./login.css');
+    }, []);
 
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(false);
 
-    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
+    const [errorData, setErrorData] = useState('');
+
+    const emailValidPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordValidPattern = /^(?=.*[0-9])(?=.*[!@#$%])[a-zA-Z0-9!@#$%]*$/;
+
+    function addErrorClass (target: HTMLInputElement, ifCorrect: boolean){
+        if(ifCorrect){
+
+            target.classList.remove("errorClass")
+            target.classList.add("noterrorClass")
+        }
+        else{
+            target.classList.add("errorClass");
+            target.classList.remove("noterrorClass")
+        }
+    }
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+        const isValid: boolean = emailValidPattern.test(e.target.value) && e.target.value.length > 5;
+        addErrorClass(e.target, isValid);
     };
 
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(e.target.value);
+        const isValid: boolean = passwordValidPattern.test(e.target.value) && e.target.value.length > 7;
+        addErrorClass(e.target, isValid);
     };
 
     const handleRememberMeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,9 +47,16 @@ export default function LoginSite() {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('Username:', username);
-        console.log('Password:', password);
-        console.log('Remember me:', rememberMe);
+        if(
+            emailValidPattern.test(email) && email.length > 5 &&
+            passwordValidPattern.test(password) && password.length > 7
+        ){
+            setErrorData("");
+            // axios sends data to backend
+        }
+        else{
+            setErrorData("Error message");
+        }
     };
 
     return (
@@ -32,13 +64,11 @@ export default function LoginSite() {
             <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input
-                        id='login'
-                        type="text"
-                        placeholder="Username"
-                        required
-                        value={username}
-                        onChange={handleUsernameChange}
+                    <input type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
                     />
                     <i className='bx bxs-user'></i>
                 </div>
@@ -65,11 +95,15 @@ export default function LoginSite() {
                     </label>
                     <a href="#">Forgot password?</a>
                 </div>
-
+                <div className='errordata'>
+                    <p>{errorData}</p>
+                </div>
                 <button type="submit" className="btn">Login</button>
                 <div className="register-link">
                     <p>Don't have an account? <Link to="/register">Register</Link></p>
+                    <p>Don't wanna be here? <a href="/">Kill your self</a></p>
                 </div>
+                
             </form>
         </div>
     );
